ff1fd2bc5c243480d3aa9112ac47312419673a7e
pbabu1 <pbabu1@lenovo.com>
2019-01-09 14:12:58 +0530

    more scripts

---
+++ numbsear.py
@@ -0,0 +1,19 @@
+i = [1, 2, 3, 4, 5]
+j=1
+# print(i[0])
+print(len(i))
+for y in i:
+    print(y)
+    if y==4:
+        #print(i[y])
+        print ("found")
+        # break
+    else:
+        print ("not found")
+        # break
+
+for x in i:
+#if i==j:
+    print(i)
+    # if i==2:
+     #   break
